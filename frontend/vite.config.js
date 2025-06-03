5ffc5e2220ac9c084cf786a4c475f679164a038f
Ajay <ajayxa1@gmail.com>
2025-06-03 23:06:19 +0530

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+  build: {
+    outDir: "../backend/public", // outputs directly to backend/public
+    emptyOutDir: true,
+  },
   plugins: [tailwindcss()],
 });
