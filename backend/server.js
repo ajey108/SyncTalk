129656164096071ce85da6301691103bcdee6a90
Ajay <ajayxa1@gmail.com>
2025-06-04 22:20:48 +0530

    update server.js commented out cors

--- backend/server.js
+++ backend/server.js
@@ -34,18 +34,18 @@ console.log("allowed orgs", allowedOrigins);
 const io = new Server(server);
 
 // CORS configuration
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 
 app.use(cookieParser());
 app.use(express.json());
